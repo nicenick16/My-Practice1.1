41494e91370222941fce4e0fe5dd80dcaee13350
Nikhil Gupta <gnikhil1604@gmail.com>
2023-03-31 13:51:25 +0530

    automation site opened and clicked on switch to than on iframe

--- src/pkg1/Iframe.java
+++ src/pkg1/Iframe.java
@@ -1,5 +1,25 @@
 package pkg1;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Iframe {
+	
+	public static void main(String[]args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.automationtesting.in/Register.html");
+		
+		WebElement switchto = driver.findElement(By.xpath("(//*[@href=\"SwitchTo.html\"])[1]"));
+		switchto.click();
+		WebElement iframe = driver.findElement(By.xpath("//*[@href=\"Frames.html\"]"));
+		iframe.click();
+	}
+
 
 }
